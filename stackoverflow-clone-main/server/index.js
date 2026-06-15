05486cf904fe7b855ae056aeba90318310d0d052
Srijith S <srijith11.2006@gmail.com>
2026-06-15 11:39:34 +0530

    fix: allow all vercel.app subdomains in CORS to fix preview deploys

--- stackoverflow-clone-main/server/index.js
+++ stackoverflow-clone-main/server/index.js
@@ -18,10 +18,17 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://project-gufi3-d5x3i45ir-srijith112006-2265s-projects.vercel.app",
-  ],
+  origin: (origin, callback) => {
+    if (
+      !origin ||
+      origin.endsWith(".vercel.app") ||
+      origin === "http://localhost:3000"
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 }));
 
